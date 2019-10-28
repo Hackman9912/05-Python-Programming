7236c084b0bddc8c2d2bb2263bef1d21b136d7d1
Hackman9912 <charleshackett89@gmail.com>
2019-10-28 14:55:59 -0500

    AUTOMATED UPDATE

--- 02_Data_Types/performance_labs/Lab_2D_2E/Lab2E.py
+++ 02_Data_Types/performance_labs/Lab_2D_2E/Lab2E.py
@@ -6,8 +6,8 @@ user_input=input("Input your text with spaces to separate words! ")
 user_input.split(" ")
 
 a = user_input.split(" ")
-print("Your sentence is", len(a), "words long and", len(user_input), "characters long! ")
+print(" Your sentence is", len(a), "words long and", len(user_input), "characters long! ")
 
-letter_input=input("Put a letter in to see how many show up in your sentence ")
+letter_input=input(" Put a letter in to see how many show up in your sentence ")
 
 print(user_input.count(letter_input))
