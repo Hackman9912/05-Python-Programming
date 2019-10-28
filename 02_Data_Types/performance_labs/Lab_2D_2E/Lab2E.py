a01c9518be46ffea1a46b170007ca1cc2d75d99e
Hackman9912 <charleshackett89@gmail.com>
2019-10-28 14:22:08 -0500

    AUTOMATED UPDATE

---
+++ 02_Data_Types/performance_labs/Lab_2D_2E/Lab2E.py
@@ -0,0 +1,9 @@
+# Write a program that takes a string as user input then counts the number of words in 
+# that sentence.
+
+user_input=input("Input your text! ")
+
+user_input.split(" ")
+
+a = user_input.split(" ")
+print("Your sentence is", len(a), "words long and", len(user_input), "characters long! ")
