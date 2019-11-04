90c6c4581bc7be8879e463f89a7139632ddf1725
Hackman9912 <charleshackett89@gmail.com>
2019-11-04 11:27:16 -0600

    AUTOMATED UPDATE

--- Additional Stuff/Lists/listFunctions.py
+++ Additional Stuff/Lists/listFunctions.py
@@ -142,3 +142,140 @@ my_list = [5, 4, 7, 8, 3, 2, 0, 9, 1]
 print("The lowest value is: ", min(my_list))
 
 print('The highest value is: ', max(my_list))
+
+
+# copyting lists in python
+
+list1 = [1, 2, 3, 4, 5]
+
+
+# assign the list to the list to variable
+list2 = list1
+
+print(list1)
+print(list2)
+
+# Replace part of the list
+
+list1[0] = 99
+print(list1)
+print(list2)
+
+# copoy of the list that references two separate but identical lists
+
+list3 = [1, 2, 3, 4, 5]
+list4 = []
+
+print("OG list 3", list3)
+print("OG 4", list4)
+
+for item in list3:
+    list4.append(item)
+
+print("step 2", list3)
+print("step 2", list4)
+
+list3[0] = 99
+
+print("step 3", list3)
+print("step 3", list4)
+
+# pass list to an argument as a funciton
+
+# calc total value of the list
+
+def main():
+    numbers = [2, 4, 6, 8, 10]
+    print("The total is", get_total(numbers))
+
+
+# The get_total function accepts a list as an argument
+
+def get_total(value_list):
+    # Create an accumulator
+    total = 0
+
+    for num in value_list:
+        total += num
+
+    return total
+
+
+main()
+
+
+# files and lists
+
+# This program uses the writelines method to save a list of string to a file
+
+def main():
+    # Create a lis of strings
+    cities = ['New York\n', 'Austin\n', 'Boston\n', 'Atlatna\n', 'Dallas\n']
+
+    outfile = open('cities.txt', 'w')
+
+    for item in cities:
+        outfile.writelines(item)
+
+    outfile.close()
+
+main()
+
+
+# This program reads the contents
+
+def main():
+    infile = open('cities.txt', 'r')
+
+    cities_read_in = infile.readlines()
+
+    infile.close()
+
+    index = 0
+
+    while index < len(cities_read_in):
+        cities_read_in[index] = cities_read_in[index].rstrip('\n')
+        index += 1
+
+    print(cities_read_in)
+
+main()
+
+
+
+# 2 dimensional list (nested lists or lists within lists)
+
+students = [['joe', 'kim'], ['Sam', 'Sue'], ['Kelly', 'Christ']]
+print(students)
+
+print(students[0])
+# Print Joe
+print(students[0][0])
+# Print Sue
+print(students[1][1])
+
+
+# This program assigns random numbers to a 2-D list
+
+import random
+
+
+# Constants for rows an collumnds
+
+ROWS = 3
+COLUMNS = 4
+
+def main():
+    # Create 2D list
+    values = [[0, 0, 0 , 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]]
+    
+    # Fill the list with random numbers
+    for r in range(ROWS):
+        for c in range(COLUMNS):
+            values[r][c] = random.randint(1, 100)
+
+    print(values)
+
+main()
