f0b10754402e4dcc59592380a78756527ff31da6
Hackman9912 <charleshackett89@gmail.com>
2019-11-12 10:30:40 -0600

    AUTOMATED UPDATE

---
+++ Additional Stuff/Classes/classes.py
@@ -0,0 +1,7 @@
+# Classes
+# Code that specifies data attributes and methods for a particular type of object
+
+# Classes use CamelCasing
+
+# self parameter is required in every class and function definition
+
