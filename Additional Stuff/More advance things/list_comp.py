4f143ddd69ca4b3e4e0c7332eea0af472c03cc4d
Hackman9912 <charleshackett89@gmail.com>
2019-11-15 14:56:18 -0600

    AUTOMATED UPDATE

--- Additional Stuff/More advance things/list_comp.py
+++ Additional Stuff/More advance things/list_comp.py
@@ -22,18 +22,31 @@
 # print(combined_list)
 
 # list comprehension with optional predicate
-evens = [x for x in range(21) if x % 2 == 0]
-print(evens)
+# evens = [x for x in range(21) if x % 2 == 0]
+# print(evens)
 
-# list comprehension with multiple optional predicate
-numbers = [x for x in range(21) if x % 2 == 0 or x % 5 == 0]
-print(numbers)
+# # list comprehension with multiple optional predicate
+# numbers = [x for x in range(21) if x % 2 == 0 or x % 5 == 0]
+# print(numbers)
 
-# if else list comprehension
-obj = [ 'Even' if i % 2 == 0 else 'Odd' for i in range(10)]
-print(obj)
-print('\n' * 50)
-# flatten a list
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-flat_list = [num for row in matrix for num in row]
-print(flat_list)
+# # if else list comprehension
+# obj = [ 'Even' if i % 2 == 0 else 'Odd' for i in range(10)]
+# print(obj)
+# print('\n' * 50)
+# # flatten a list
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# flat_list = [num for row in matrix for num in row]
+# print(flat_list)
+
+nums = [1, 2, 3, 4]
+str = ['a', 'b', 'c', 'd']
+# # I want a (letter, number) pair for each letter in str and each number in nums
+
+# my_list  = []
+# for letter in str:
+#     for num in nums:
+#         my_list.append((letter, num))
+# print(my_list)
+
+my_list2 = [(letter, num) for letter in str for num in nums]
+print(my_list2)
