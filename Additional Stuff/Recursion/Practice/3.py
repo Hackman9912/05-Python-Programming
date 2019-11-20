9f1f42f04b063d51e2c0c9f9ccfe8c4e91cd258a
Hackman9912 <charleshackett89@gmail.com>
2019-11-20 13:01:48 -0600

    AUTOMATED UPDATE

--- Additional Stuff/Recursion/Practice/3.py
+++ Additional Stuff/Recursion/Practice/3.py
@@ -4,16 +4,21 @@
     n lines of asterisks on the screen, with the first line showing 1 asterisk, the second line
     showing 2 asterisks, up to the nth line which shows n asterisks.
 """
-
+# define stuff
 def stuff():
+    # make empty list
     list = []
+    # set variables
     y = 1
     ast = int(input('Enter the number of asterisk and lines that you want to see: '))
+    # pass to function
     lines(ast, list, y)
-
+# define lines
 def lines(x, list, y):
+    # if x is 0 print the list
     if x == 0:
         print(*list, sep = '\n')
+    # if not add the astericks and subtract from X
     else:
         list.append('*' * y)
         return lines(x - 1, list, y + 1)
--- Additional Stuff/Recursion/Practice/7.py
+++ Additional Stuff/Recursion/Practice/7.py
@@ -1,46 +1,25 @@
 """
-1. Recursive Printing
-    Design a recursive function that accepts an integer argument, n, and prints the numbers 1
-    up through n.
-
-2. Recursive Multiplication
-    Design a recursive function that accepts two arguments into the parameters x and y. The
-    function should return the value of x times y. Remember, multiplication can be performed
-    as repeated addition as follows:
-    7 X 4 = 4 + 4 + 4 + 4 + 4 + 4 + 4
-    (To keep the function simple, assume that x and y will always hold positive nonzero
-    integers.)
-
-3. Recursive Lines
-    Write a recursive function that accepts an integer argument, n. The function should display
-    n lines of asterisks on the screen, with the first line showing 1 asterisk, the second line
-    showing 2 asterisks, up to the nth line which shows n asterisks.
-
-4. Largest List Item
-    Design a function that accepts a list as an argument, and returns the largest value in the list.
-    The function should use recursion to find the largest item.
-
-5. Recursive List Sum
-    Design a function that accepts a list of numbers as an argument. The function should recursively 
-    calculate the sum of all the numbers in the list and return that value.
-
-6. Sum of Numbers
-    Design a function that accepts an integer argument and returns the sum of all the integers from 1 up 
-    to the number passed as an argument. For example, if 50 is passed as an argument, the function will 
-    return the sum of 1, 2, 3, 4, . . . 50. Use recursion to calculate the sum.
-
 7. Recursive Power Method
     Design a function that uses recursion to raise a number to a power. The function should
     accept two arguments: the number to be raised and the exponent. Assume that the exponent is a 
     nonnegative integer.
-
-8. Ackermann’s Function
-    Ackermann’s Function is a recursive mathematical algorithm that can be used to test how
-    well a system optimizes its performance of recursion. Design a function ackermann(m, n),
-    which solves Ackermann’s function. Use the following logic in your function:
-        If m = 0 then return n  1
-        If n = 0 then return ackermann(m - 1, 1)
-        Otherwise, return ackermann(m - 1, ackermann(m, n - 1))
-    Once you’ve designed your function, test it by calling it with small values for m and n.
-
 """
+# define main
+def main():
+    # Establish vars
+    int1 = int(input("Enter the first integer: "))
+    int2 = int(input("Enter the second integer: "))
+    count = 1
+    tot = 1
+    # pass to function
+    exp(int1, int2, count, tot)
+# define exponent
+def exp(x, y, z, a):
+    # if z is greater than the exponent then print the formatted number
+    if z > y:
+        print(f'{a:,.2f}')
+    # If not cal lthe function again passing z, y and z plus 1 and the total times the base number 
+    else:
+        return exp(x, y, z + 1, a * x)
+# call main
+main()
