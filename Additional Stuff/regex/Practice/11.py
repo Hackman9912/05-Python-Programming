f236ccf737a2a962de6701be8fc1daebf6ca4555
Hackman9912 <charleshackett89@gmail.com>
2019-11-21 15:30:36 -0600

    AUTOMATED UPDATE

--- Additional Stuff/regex/Practice/11.py
+++ Additional Stuff/regex/Practice/11.py
@@ -5,3 +5,21 @@ the months January to September (0?[1-9]). Create the regex
 that represents the remaining three months in the standard
 calendar.
 """
+import re
+months = '''01
+2
+03
+04
+05
+06
+07
+08
+09
+10
+11
+12
+'''
+pattern = re.compile(r'(\b(0?[1-9])\b|\b(1[1-2])\b)') 
+matches = pattern.finditer(months)
+for match in matches:
+    print(match)
